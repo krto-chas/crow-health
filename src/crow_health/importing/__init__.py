4dc3b5cd7645456bfdcf67b0d0e9c64b7ee4c578
Kristoffer Toivanen <kristoffer.toivanen@chasacademy.se>
2026-08-04 22:35:00 +0200

    Expose batch import API

--- src/crow_health/importing/__init__.py
+++ src/crow_health/importing/__init__.py
@@ -1,4 +1,18 @@
+from crow_health.importing.batch import (
+    BatchImportReport,
+    BatchImportService,
+    BatchMemberReport,
+    discover_zip_members,
+)
 from crow_health.importing.pipeline import ImportReport, ImportService
 from crow_health.importing.sources import load_json_zip_member
 
-__all__ = ["ImportReport", "ImportService", "load_json_zip_member"]
+__all__ = [
+    "BatchImportReport",
+    "BatchImportService",
+    "BatchMemberReport",
+    "ImportReport",
+    "ImportService",
+    "discover_zip_members",
+    "load_json_zip_member",
+]
