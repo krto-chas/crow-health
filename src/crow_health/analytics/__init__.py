f33b16bf560b9d5a6fa666f873290091c70534be
Kristoffer Toivanen <kristoffer.toivanen@chasacademy.se>
2026-08-05 21:14:25 +0200

    Expose analytics API

---
+++ src/crow_health/analytics/__init__.py
@@ -0,0 +1,23 @@
+from crow_health.analytics.models import (
+    AnalyticsQuery,
+    CompletenessResult,
+    MovingAveragePoint,
+    MovingAverageResult,
+    OutlierPoint,
+    OutlierResult,
+    TrendDirection,
+    TrendResult,
+)
+from crow_health.analytics.service import AnalyticsService
+
+__all__ = [
+    "AnalyticsQuery",
+    "AnalyticsService",
+    "CompletenessResult",
+    "MovingAveragePoint",
+    "MovingAverageResult",
+    "OutlierPoint",
+    "OutlierResult",
+    "TrendDirection",
+    "TrendResult",
+]
