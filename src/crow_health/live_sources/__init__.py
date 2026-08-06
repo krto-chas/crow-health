06dfa8ac9784a256dc324562f5cc6577eee694c2
Kristoffer Toivanen <kristoffer.toivanen@chasacademy.se>
2026-08-06 20:32:25 +0200

    Expose live-source feasibility report

---
+++ src/crow_health/live_sources/__init__.py
@@ -0,0 +1,13 @@
+from crow_health.live_sources.garmin_feasibility import (
+    FeasibilityStatus,
+    GarminLiveSourceReport,
+    SourceCandidate,
+    garmin_live_source_report,
+)
+
+__all__ = [
+    "FeasibilityStatus",
+    "GarminLiveSourceReport",
+    "SourceCandidate",
+    "garmin_live_source_report",
+]
